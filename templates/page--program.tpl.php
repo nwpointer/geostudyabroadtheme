3466b89b4d7360ec7dafa91fdbdc606fdd1ae757
Nathan Pointer <nathanpointer@vpn174-110.uoregon.edu>
2015-05-08 07:37:11 -0700

    updated favorites copy

--- templates/page--program.tpl.php
+++ templates/page--program.tpl.php
@@ -244,7 +244,7 @@ jQuery("#secondary-menu").toggle();});
 
       favoritesList = new persistantList({
           el: '#favorites-list',
-          template : "<h4>favorited programs</h4>{{#each favorites: i}}{{>item}}{{/each}}",
+          template : "<h4>favorites</h4>{{#each favorites: i}}{{>item}}{{/each}}",
           add: function(fav){
               this.get("favorites").push(fav);
               this.toggle('includesThisPage');
