930d7aa257e494fe5e1e62628649d2f2714b69b2
Intl Afairs Web Assistant <iawebassist@d38-245.uoregon.edu>
2015-05-15 13:53:31 -0700

    scholarship testbed

---
+++ templates/node--459.tpl.php
@@ -0,0 +1,75 @@
+<ul id="scholarship-container">
+	
+<li>
+	<h3><a href="/">GEO Scholarship</a></h3>
+	<p>
+		GEO is passionate about study abroad and hopes that every student is able to participate in a life-...
+	</p>
+</li>
+
+<a href="/">Read More</a>
+
+<li>
+	<h3><a href="/">General Study Abroad Scholarships</a></h3>
+	<p>
+		These scholarships are open to a majority of students or are not tied to a specific program or region....
+	</p>
+</li>
+
+<a href="/">Read More</a>
+
+<li>
+	<h3><a href="/">UO Departmental Scholarships</a></h3>
+	<p>
+		Several UO departments, associations, and colleges offer study abroad scholarships.
+	</p>
+</li>
+
+<a href="/">Read More</a>
+
+<li>
+	<h3><a href="/">National Distinguished Scholarships</a></h3>
+	<p>
+		Prestigious national scholarships that support undergraduate and graduate students conducting research or...
+	</p>
+</li>
+
+<a href="/">Read More</a>
+
+<li>
+	<h3><a href="/">Regional and Program Provider Scholarships</a></h3>
+	<p>
+		Many of the UO-sponsored study abroad providers offer scholarships for their programs. There are also...
+	</p>
+</li>
+
+<a href="/">Read More</a>
+
+<li>
+	<h3><a href="/">Using Your Financial Aid for Study Abroad</a></h3>
+	<p>
+		Most forms of financial aid may be used to pay for your study abroad program costs. Please note, however,...
+	</p>
+</li>
+
+<a href="/">Read More</a>
+
+<li>
+	<h3><a href="/">External Scholarship Resources</a></h3>
+	<p>
+		The following guide includes state, national, and international financial aid resources. Some of these...
+	</p>
+</li>
+
+<a href="/">Read More</a>
+
+<li>
+	<h3><a href="/">Resources for graduating seniors and graduate students</a></h3>
+	<p>
+		The following listing can help graduate students find many fellowship, scholarship, and grant options for...
+	</p>
+</li>
+
+<a href="/">Read More</a>
+
+</ul>
