3eb164d463d92736b7b799702a9f52a6799e8c9a
Intl Afairs Web Assistant <iawebassist@d38-180.uoregon.edu>
2015-05-26 15:59:57 -0700

    scholarship link fix

--- templates/node--645.tpl.php
+++ templates/node--645.tpl.php
@@ -22,7 +22,7 @@
 
 
 <li>
-	<h3 style="border-color:#62a710 !important"><a href="/uo_only/dpt">UO Departmental Scholarships</a></h3>
+	<h3 style="border-color:#62a710 !important"><a href="/scholarships/uo_only/dpt">UO Departmental Scholarships</a></h3>
 	<p>
 		Several UO departments, associations, and colleges offer study abroad scholarships.
 	</p>
@@ -31,7 +31,7 @@
 
 
 <li>
-	<h3 style="border-color:#daaa00 !important"><a href="/uo_only/distinguished">National Distinguished Scholarships</a></h3>
+	<h3 style="border-color:#daaa00 !important"><a href="/scholarships/uo_only/distinguished">National Distinguished Scholarships</a></h3>
 	<p>
 		Prestigious national scholarships that support undergraduate and graduate students conducting research or...
 	</p>
@@ -40,7 +40,7 @@
 
 
 <li>
-	<h3 style="border-color:#007934 !important"><a href="/uo_only/regional_specific">Regional and Program Provider Scholarships</a></h3>
+	<h3 style="border-color:#007934 !important"><a href="/scholarships/uo_only/regional_specific">Regional and Program Provider Scholarships</a></h3>
 	<p>
 		Many of the UO-sponsored study abroad providers offer scholarships for their programs. There are also...
 	</p>
@@ -49,7 +49,7 @@
 
 
 <li>
-	<h3 style="border-color:#693025 !important"><a href="/uo_only/financial_aid">Using Your Financial Aid for Study Abroad</a></h3>
+	<h3 style="border-color:#693025 !important"><a href="/scholarships/uo_only/financial_aid">Using Your Financial Aid for Study Abroad</a></h3>
 	<p>
 		Most forms of financial aid may be used to pay for your study abroad program costs. Please note, however,...
 	</p>
@@ -58,7 +58,7 @@
 
 
 <li>
-	<h3 style="border-color:#55565b !important"><a href="/external">External Scholarship Resources</a></h3>
+	<h3 style="border-color:#55565b !important"><a href="/scholarships/external">External Scholarship Resources</a></h3>
 	<p>
 		The following guide includes state, national, and international financial aid resources. Some of these...
 	</p>
@@ -67,7 +67,7 @@
 
 
 <li>
-	<h3 style="border-color:#004e72 !important"><a href="/uo_only/graduates">Resources for graduating seniors and graduates</a></h3>
+	<h3 style="border-color:#004e72 !important"><a href="/scholarships/uo_only/graduates">Resources for graduating seniors and graduates</a></h3>
 	<p>
 		The following listing can help graduate students find many fellowship, scholarship, and grant options for...
 	</p>
